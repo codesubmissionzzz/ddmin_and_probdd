32fde8dfd64aad7a22a60074b33dea5780063c46
codesubmissionzzz <codesubmissionzzz@gmail.com>
2023-05-16 12:18:00 -0400

    cleanup

--- benchmarks/chisel-bench/98e01697_algorithm_probdd_id_0/process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-input_file="log_chisel_date-8.21"
-output_file="output.txt"
-
-awk '{$1=$2=""; print substr($0, 3)}' "$input_file" > "$output_file"
-
--- benchmarks/chisel-bench/benchmark/lib/memwrap.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void *malloc(size_t size) { return NULL; }
-
-void *realloc(void *ptr, size_t size) { return NULL; }
-
-void *calloc(size_t num, size_t size) { return NULL; }
